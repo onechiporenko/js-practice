bab29dad3bb4adca9a388d99b38800d098dd53fc
Oleg Nechiporenko <onechiporenko@apache.org>
2015-06-18 18:51:49 +0300

    Remove solution

--- app/controllers/solutions/arguments.js
+++ app/controllers/solutions/arguments.js
@@ -6,11 +6,6 @@ export default Ember.Controller.extend({
 
   execSecondParameter: Ember.B,
 
-  callIfExists: function () {
-    var func = arguments[0];
-    if(Ember.typeOf(Math[func]) === 'function') {
-      Math[func].apply(Math, Array.prototype.slice.call(arguments, 1));
-    }
-  }
+  callIfExists: Ember.B
 
 });
