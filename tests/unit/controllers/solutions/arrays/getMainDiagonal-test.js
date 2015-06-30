8e0a282292d4dc13fc9fcd5dba8606bab38358a3
Oleg Nechiporenko <onechiporenko@apache.org>
2015-06-30 22:35:25 +0300

    Remove 1 test

--- tests/unit/controllers/solutions/arrays/getMainDiagonal-test.js
+++ tests/unit/controllers/solutions/arrays/getMainDiagonal-test.js
@@ -4,15 +4,10 @@ moduleFor('controller:solutions/arrays', 'solutions.arrays.getMainDiagonal', {})
 
 test('test1', function(assert) {
   var controller = this.subject();
-  assert.deepEqual(controller.getMainDiagonal([[]]), [], `([[]]) -> []`);
-});
-
-test('test2', function(assert) {
-  var controller = this.subject();
   assert.deepEqual(controller.getMainDiagonal([[1,2],[3,4]]), [1,4], `([[1,2],[3,4]]) -> [1,4]`);
 });
 
-test('test3', function(assert) {
+test('test2', function(assert) {
   var controller = this.subject();
   assert.deepEqual(controller.getMainDiagonal([[1,2,3],[4,5,6],[7,8,9]]), [1,5,9], `([[1,2,3],[4,5,6],[7,8,9]]) -> [1,5,9]`);
 });
