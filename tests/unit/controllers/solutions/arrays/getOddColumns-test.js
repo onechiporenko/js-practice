519a848005c89b218008295c64966cd1724a694e
Oleg Nechiporenko <onechiporenko@apache.org>
2015-07-01 13:46:10 +0300

    Remove 1 test

--- tests/unit/controllers/solutions/arrays/getOddColumns-test.js
+++ tests/unit/controllers/solutions/arrays/getOddColumns-test.js
@@ -4,15 +4,10 @@ moduleFor('controller:solutions/arrays', 'solutions.arrays.getOddColumns', {});
 
 test('test1', function(assert) {
   var controller = this.subject();
-  assert.deepEqual(controller.getOddColumns([[]]), [], `([[]]) -> []`);
-});
-
-test('test2', function(assert) {
-  var controller = this.subject();
   assert.deepEqual(controller.getOddColumns([[1,2],[3,4]]), [[1],[3]], `([[1,2],[3,4]]) -> [[1],[3]]`);
 });
 
-test('test3', function(assert) {
+test('test2', function(assert) {
   var controller = this.subject();
   assert.deepEqual(controller.getOddColumns([[1,2,3],[4,5,6],[7,8,9],[10,11,12]]), [[1,3],[4,6],[7,9],[10,12]], `([[1,2,3],[4,5,6],[7,8,9],[10,11,12]]) -> [[1,3],[4,6],[7,9],[10,12]]`);
 });
