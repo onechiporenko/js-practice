dffbcd138f4a482c1726d78e42d754c8cfa1822d
Oleg Nechiporenko <onechiporenko@apache.org>
2015-08-16 16:31:01 +0300

    Fix method name

--- app/controllers/solutions/bits.js
+++ app/controllers/solutions/bits.js
@@ -4,18 +4,6 @@ export default Ember.Controller.extend({
 
   checkBit: Ember.B,
 
-  checkNumberBinaryForm: function(a) {
-    var nearestPower2 = Math.floor(Math.log2(a));
-    var fl = true;
-    for (var i = nearestPower2; i >= 0 ; i--) {
-      if (!(fl ^ (a & Math.pow(2, i)) > 0)) {
-        fl = !fl;
-      }
-      else {
-        return false;
-      }
-    }
-    return true;
-  }
+  checkNumberBinaryFormWithBits: Ember.B
 
 });
